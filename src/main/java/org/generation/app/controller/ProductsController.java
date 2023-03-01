8a2e65b08a5ae2273d1b3988f8937dfcd5cd5df7
Alejandro Morales <alex94morales@hotmail.com>
2023-03-01 15:20:30 -0600

    prueba

--- src/main/java/org/generation/app/controller/ProductsController.java
+++ src/main/java/org/generation/app/controller/ProductsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/products")
 public class ProductsController {
-	
+	//prueba de commit
 	@Autowired
 	IProductsService productsService;
 	
