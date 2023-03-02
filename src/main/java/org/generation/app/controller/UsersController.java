5aa011283c67594e2be3ca466a78386c68217ec8
Fernanda Rosales <fernanda.rosales.ortega@gmail.com>
2023-03-01 21:14:10 -0600

    update and put ready

--- src/main/java/org/generation/app/controller/UsersController.java
+++ src/main/java/org/generation/app/controller/UsersController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.generation.app.model.Users;
 import org.generation.app.service.IUsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +33,32 @@ public class UsersController {
 		return usersService.getAllUsers();
 		
 	}
+	
+	@PostMapping //localhost:8080/api/customers
+    public ResponseEntity<?> setNewUsers(@RequestBody Users users) {
+        try {
+            return new ResponseEntity<Users>(
+                    usersService.setUser(users), 
+                    HttpStatus.CREATED);
+
+        } catch (IllegalStateException e) {
+            return new ResponseEntity<String>(e.getMessage() , HttpStatus.BAD_REQUEST );
+        }
+
+    }
+	
+	@PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody Users user ){
+        try {
+            return new ResponseEntity<Users>(
+                    usersService.updateUser(user), 
+                    HttpStatus.CREATED);
+
+        } catch (IllegalStateException e) {
+            return new ResponseEntity<String>(e.getMessage() , HttpStatus.BAD_REQUEST );
+        }
+
+
+    }
 
 }
