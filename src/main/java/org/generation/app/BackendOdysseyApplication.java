35caddf110bf8c4f9dd5caee279616bd4edb6492
Fernanda Rosales <fernanda.rosales.ortega@gmail.com>
2023-03-06 19:46:11 -0600

    agregar crossOrigin

--- src/main/java/org/generation/app/BackendOdysseyApplication.java
+++ src/main/java/org/generation/app/BackendOdysseyApplication.java
@@ -2,8 +2,11 @@ package org.generation.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class BackendOdysseyApplication {
 
 	public static void main(String[] args) {
